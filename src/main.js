02d508d4db5453b366410cffa0146a9ff3aa9646
origami-owo <hanserr@vip.qq.com>
2025-07-18 18:56:49 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import './assets/tailwind.css'
 import App from './App.vue'
 
-createApp(App)     // ✅ 挂载的是 root.vue
+createApp(App)
   .mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: './',
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/api/cards': {
-        target: 'https://storage.my-api.cn',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api\/cards/, '/static/images/Cards')
-      }
-    }
-  }
 })
